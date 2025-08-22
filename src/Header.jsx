be457f76f13982195189b30661b7dd13b3ba14f8
Lucas Carlos Dante <lucascarlos.rocha@gmail.com>
2025-08-22 00:43:42 -0300

    Ajuste header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+
+import React, { useEffect } from 'react';
 import './App.css';
 import { ENV, ENV_COLORS } from './env';
 
 function Header() {
+  useEffect(() => {
+    document.title = `Ambiente: ${ENV.toUpperCase()}`;
+  }, []);
   return (
     <header style={{ background: ENV_COLORS[ENV], color: '#fff', padding: '1rem', textAlign: 'center' }}>
       Ambiente: <b>{ENV.toUpperCase()}</b>
